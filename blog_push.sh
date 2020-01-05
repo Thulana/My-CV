06c56515a03053ea9a2066586d37b3bca81d49fd
thulana <Thulana.Kannangara@sysco.com>
2020-01-05 22:11:29 +0530

    travis - give permission to scripts

--- blog_push.sh
+++ blog_push.sh
@@ -6,7 +6,8 @@ commit_website_files() {
   cp ./cv.pdf ./thulana.github.io/assets/cv.pdf
   cd ./thulana.github.io && git add assets/cv.pdf
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER - Pushing updated cv to the blog"
-  git push https://"${GITHUB_USER}":"${GITHUB_PASSWORD}"@thulana.github.io.git --all
+#  git push https://"${GITHUB_USER}":"${GITHUB_PASSWORD}"@thulana.github.io.git --all
+  git push origin master
 }
 
 
