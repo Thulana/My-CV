83a7e7dfe9d9366fa2f712b640772b64b3f7f093
thulana <Thulana.Kannangara@sysco.com>
2020-01-05 22:59:22 +0530

    travis - change gh auth - 2

--- blog_push.sh
+++ blog_push.sh
@@ -10,12 +10,12 @@ commit_website_files() {
   cp ./cv.pdf ./thulana.github.io/assets/cv.pdf
   cd ./thulana.github.io && git add assets/cv.pdf
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER - Pushing updated cv to the blog"
-#  git push https://"${GITHUB_USER}":"${GITHUB_PASSWORD}"@thulana.github.io.git --all
-  git push https://"${GH_TOKEN}"@github.com/Thulana/thulana.github.io.git master
+  git push https://"${GITHUB_USER}":"${GITHUB_PASSWORD}"@thulana.github.io.git --all
+#  git push https://"${GH_TOKEN}"@github.com/Thulana/thulana.github.io.git master
 }
 
 
 echo "pushing to my blog"
-setup_git
+#setup_git
 commit_website_files
 #upload_files
